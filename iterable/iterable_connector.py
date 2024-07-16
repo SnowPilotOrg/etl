c36bcbfecb04d007eb53855a07f791de124e288d
Ben Warren <warrenbhw@gmail.com>
2024-07-15 23:21:27 -0700

    iterable test - simple request

--- iterable/iterable_connector.py
+++ iterable/iterable_connector.py
@@ -1,11 +1,32 @@
-"""
-Stub methods for the CSV connector.
-"""
+import httpx
 
 
 def discover():
     """Discover available streams and their schemas."""
     print("hello discover")
+    headers = {
+        "Api-Key": "fake",
+        "Content-Type": "application/json",
+    }
+    response = httpx.post(
+        "https://api.iterable.com/api/users/update",
+        headers=headers,
+        json={
+            "email": "jack@example.com",
+            "userId": "2345",
+            "dataFields": {},
+            "preferUserId": True,
+            "mergeNestedObjects": True,
+            "createNewFields": True,
+        },
+    )
+    print(response.json())
+
+    # )
+    # conn.request("POST", "/api/users/update", payload, headers)
+    # res = conn.getresponse()
+    # data = res.read()
+    # print(data.decode("utf-8"))
 
 
 def extract(stream_id, fields):
