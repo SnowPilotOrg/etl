951fc24937f08fd2f88fccfe598a30eaed55c083
Ben Warren <warrenbhw@gmail.com>
2024-07-17 00:04:52 -0700

    more snowflake scaffolding

--- snowflake/snowflake/main.py
+++ snowflake/snowflake/main.py
@@ -11,7 +11,7 @@ from rich import print
 from rich.console import Console
 from typing_extensions import Annotated
 
-from snowflake_connector.models import (
+from snowflake.models import (
     Catalog,
     CollectionMetadata,
     Config,
