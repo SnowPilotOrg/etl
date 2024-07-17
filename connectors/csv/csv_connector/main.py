eedd7ef361601e6ffa316a5a6359841a247903f7
Ben Warren <warrenbhw@gmail.com>
2024-07-16 17:20:44 -0700

    start using rich

--- connectors/csv/csv_connector/main.py
+++ connectors/csv/csv_connector/main.py
@@ -1,26 +1,33 @@
 from typing import Optional
 
+import jsonc
 import typer
+from rich import print
+from rich.console import Console
 from typing_extensions import Annotated
 
+err_console = Console(stderr=True)
 app = typer.Typer()
 
 
 @app.command()
 def discover(
-    config: Annotated[str, typer.Option(help="Path to the configuration file")],
+    config_path: Annotated[str, typer.Option(help="Path to the configuration file")],
 ):
     """Discover available streams and their schemas."""
     typer.echo("Discovering available streams and their schemas")
+    with open(config_path, "r") as file:
+        config = jsonc.load(file)
+    print(config)
 
 
 @app.command()
 def extract():
     """Extract data from the specified stream."""
-    typer.echo("Extracting data from the specified stream")
+    err_console.print("Extract is not implemented yet")
 
 
 @app.command()
 def load():
     """Load data into the specified stream."""
-    typer.echo("Loading data into the specified stream")
+    err_console.print("Load is not implemented yet")
